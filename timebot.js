a2dd6fa290398085b8c6a4c3c8c8948ec7502a46
Makiv-Vik <71525436+Makiv-Vik@users.noreply.github.com>
2021-02-08 23:55:39 +0200

    mel

--- timebot.js
+++ timebot.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const { Telegraf } = require('telegraf');
-const bot = new Telegraf(BOT_TOKEN);
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const Markup = require('telegraf/markup');
 
